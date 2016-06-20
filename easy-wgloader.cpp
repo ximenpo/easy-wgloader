0a38910f89f3fd87c82ac1820d17f194f33a3534
ximenpo <ximenpo@jiandan.ren>
2016-06-20 13:35:46 +0800

    initial files.

---
+++ easy-wgloader.cpp
@@ -0,0 +1,34 @@
+// easy-wgloader.cpp : 定义应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "easy-wgloader.h"
+
+#define MAX_LOADSTRING 100
+
+// 全局变量:
+HINSTANCE hInst;								// 当前实例
+TCHAR szTitle[MAX_LOADSTRING];					// 标题栏文本
+TCHAR szWindowClass[MAX_LOADSTRING];			// 主窗口类名
+
+// 此代码模块中包含的函数的前向声明:
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	// 初始化全局字符串
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_EASYWGLOADER, szWindowClass, MAX_LOADSTRING);
+
+	_Module.Init(NULL, hInstance);
+
+
+	_Module.Term();
+
+	return (int) EXIT_SUCCESS;
+}
