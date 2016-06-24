9553df07124984715277a7a34f52948c7ff1191a
ximenpo <ximenpo@jiandan.ren>
2016-06-24 11:08:07 +0800

    impl ShowContextMenu method.

--- IECustomizer.cpp
+++ IECustomizer.cpp
@@ -12,13 +12,41 @@ IECustomizer::~IECustomizer(void)
 {
 }
 
+    //·µ»ØS_OK£¬ÆÁ±ÎµôÓÒ¼ü²Ëµ¥
+HRESULT STDMETHODCALLTYPE IECustomizer::ShowContextMenu(
+        /* [in] */ DWORD dwID,
+        /* [in] */ POINT __RPC_FAR *ppt,
+        /* [in] */ IUnknown __RPC_FAR *pcmdtReserved,
+        /* [in] */ IDispatch __RPC_FAR *pdispReserved){
+	if(g_param.debug){
+		return	S_FALSE;
+	}
+
+	switch( dwID )
+	{
+	case CONTEXT_MENU_CONTROL:
+	case CONTEXT_MENU_TEXTSELECT:
+	case CONTEXT_MENU_UNKNOWN:
+	case CONTEXT_MENU_VSCROLL:
+	case CONTEXT_MENU_HSCROLL:
+		return S_FALSE;
+	}
+	
+	return S_OK;
+}
+
 HRESULT STDMETHODCALLTYPE IECustomizer::GetHostInfo(
     /* [out][in] */ DOCHOSTUIINFO __RPC_FAR *pInfo) {
 	if(g_param.debug){
 		return	S_FALSE;
 	}
 
-    pInfo->dwFlags	|= DOCHOSTUIFLAG_DIALOG;
+    pInfo->dwFlags	|= DOCHOSTUIFLAG_DIALOG
+		|	DOCHOSTUIFLAG_ACTIVATE_CLIENTHIT_ONLY
+		|	DOCHOSTUIFLAG_NO3DBORDER
+		|	DOCHOSTUIFLAG_NO3DOUTERBORDER
+		|	DOCHOSTUIFLAG_ENABLE_REDIRECT_NOTIFICATION
+		;
     return S_OK;
 }
 
--- IECustomizer.h
+++ IECustomizer.h
@@ -10,6 +10,12 @@ public:
 	~IECustomizer(void);
 
 public:
+    //·µ»ØS_OK£¬ÆÁ±ÎµôÓÒ¼ü²Ëµ¥
+    virtual HRESULT STDMETHODCALLTYPE		ShowContextMenu(
+        /* [in] */ DWORD dwID,
+        /* [in] */ POINT __RPC_FAR *ppt,
+        /* [in] */ IUnknown __RPC_FAR *pcmdtReserved,
+        /* [in] */ IDispatch __RPC_FAR *pdispReserved);
     virtual HRESULT STDMETHODCALLTYPE		GetHostInfo(
         /* [out][in] */ DOCHOSTUIINFO __RPC_FAR *pInfo);
     virtual HRESULT STDMETHODCALLTYPE		GetOptionKeyPath(
